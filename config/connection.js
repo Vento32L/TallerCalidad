f1dad0bd087541c7aa0121bb229f6745d17958e5
Vento22L <dionisiorockkivn@gmail.com>
2023-10-02 19:56:02 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -9,9 +9,9 @@ const mysql = require('mysql');
     }*/
     const myConn = mysql.createConnection({
         host: "localhost",
-        port: "3306",
+        port: 3306,
         user: "root",
-        password: " ",
+        password: "",
         database: "taller_calidad"
     })
 
