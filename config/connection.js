4c5eed8d72dfb9ccef4222cb07f9097b99820206
Vento22L <dionisiorockkivn@gmail.com>
2023-10-02 19:37:44 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 const mysql = require('mysql');
-    data = require('./datosPrivados.json');
+    /*data = require('./datosPrivados.json');
     objectConection = {
         host: data.mysql.host,
         port: data.mysql.port,
         user: data.mysql.user,
         pass: data.mysql.pass,
         database: data.mysql.database
-    }
+    }*/
+    const myConn = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "76A*6uyrT%",
+        database: "interactivegame_nasayuwe"
+    })
 
-    const myConn = mysql.createConnection(objectConection)
+    //const myConn = mysql.createConnection(objectConection)
 
     myConn.connect((err) => {
         if(err){
